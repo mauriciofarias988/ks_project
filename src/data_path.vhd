2f173d330bbafb4529eeeba6c90377d6988d23a7
Arthur-Rinaldi <arthur-rinaldi@uergs.edu.com>
2021-01-17 16:58:35 -0300

    Update data_path.vhd

--- src/data_path.vhd
+++ src/data_path.vhd
@@ -49,49 +49,99 @@ architecture rtl of data_path is
 signal bus_a : std_logic_vector (15 downto 0); 
 signal bus_b : std_logic_vector (15 downto 0);
 signal bus_c : std_logic_vector (15 downto 0);
-
+signal ula_out : std_logic_vector (15 downto 0);
+signal a_addr : std_logic_vector (1 downto 0); 
+signal b_addr : std_logic_vector (1 downto 0);
+signal c_addr : std_logic_vector (1 downto 0);
+signal register_0 : std_logic_vector (15 downto 0);
+signal register_1 : std_logic_vector (15 downto 0);
+signal register_2 : std_logic_vector (15 downto 0);
+signal register_3 : std_logic_vector (15 downto 0);
+signal zero_op_flag : std_logic;
+signal neg_op_flag : std_logic;
+signal signed_overflow_flag : std_logic;
+signal unsigned_overflow_flag : std_logic;
 
 begin
-    zero_op <= '1' when bus_c = "0000000000000000" else '0';
+    zero_op_flag <= '1' when ula_out = "0000000000000000" else '0';
+    neg_op_flag <= '1' when ula_out(15) = '1';
+    flags :    process (flags_reg_enable,zero_op_flag,neg_op_flag,signed_overflow_flag,unsigned_overflow_flag)
+    begin
+        if (flags_reg_enable = 1) then
+            zero_op <= zero_op_flag;
+            neg_op <= neg_op_flag;
+            signed_overflow <= signed_overflow_flag;
+            unsigned_overflow <= unsigned_overflow_flag;
+    end if;
+    end process flags;
     
     ula : process(bus_a, bus_b, operation) -- ULA 
         begin
         if(operation = "00") then --SOMA
-            bus_c <= bus_a + bus_b;
-            if (bus_a(15) = '0' AND bus_b(15) = '0') AND bus_c(15) = '1' then
-                signed_overflow <= '1'; 
-            elsif (bus_a(15) = '1' AND bus_b(15) = '1') AND bus_c(15) = '0' then
-                signed_overflow <= '1';
+            ula_out <= bus_a + bus_b;
+            if (bus_a(15) = '0' AND bus_b(15) = '0') AND ula_out(15) = '1' then
+                signed_overflow_flag <= '1'; 
+            elsif (bus_a(15) = '1' AND bus_b(15) = '1') AND ula_out(15) = '0' then
+                signed_overflow_flag <= '1';
             elsif (bus_a(15) = '0' AND bus_b(15) = '1') AND (bus_a >= (NOT bus_b) - "1") then
-               unsigned_overflow <= '1';
+               unsigned_overflow_flag <= '1';
             elsif (bus_a(15) = '1' AND bus_b(15) = '0') AND ((NOT bus_a) - "1" <= bus_b) then
-               unsigned_overflow <= '1';
+               unsigned_overflow_flag <= '1';
             elsif (bus_a(15)='1' and bus_b(15)='1') then
-               unsigned_overflow <= '1'; 
+               unsigned_overflow_flag <= '1'; 
             end if;
         elsif(operation = "01") then -- SUB
-            bus_c <= bus_b - bus_a;
-            if(bus_a(15) = '0' AND bus_b(15) = '1') AND bus_c(15) = '1' then 
-            signed_overflow <= '1';     
-            elsif (bus_a(15) = '1' AND bus_b(15) = '0') AND bus_c(15) = '0' then
-            signed_overflow <= '1';    
+            ula_out <= bus_b - bus_a;
+            if(bus_a(15) = '0' AND bus_b(15) = '1') AND ula_out(15) = '1' then 
+            signed_overflow_flag <= '1';     
+            elsif (bus_a(15) = '1' AND bus_b(15) = '0') AND ula_out(15) = '0' then
+            signed_overflow_flag <= '1';    
             elsif (bus_a(15) = '0' and bus_b(15) = '0') and (bus_a >= bus_b) then
-            unsigned_overflow <= '1';
+            unsigned_overflow_flag <= '1';
             elsif (bus_a(15) = '1' and bus_b(15) = '1') and ((not bus_a) - "1" <= (not bus_b)-1) then
-            unsigned_overflow <= '1';
+            unsigned_overflow_flag <= '1';
             elsif (bus_a(15)='1' and bus_b(15)='0') then
-            unsigned_overflow <= '1'; 
+            unsigned_overflow_flag <= '1'; 
             end if;
         elsif(operation = "10") then --AND    
-            bus_c <= bus_a AND bus_b;
+            ula_out <= bus_a AND bus_b;
         else
-            bus_c <= bus_a OR bus_b; --OR 
+            ula_out <= bus_a OR bus_b; --OR 
         end if;
     end process ula;
-         
     
+    Seletor_Register_Bank : process (c_sel,ula_out,data_in)
+        begin
+        if (c_sel='0') then
+            bus_c <= data_in;
+        else
+            bus_c <= ula_out;
+        end if;
+        end process Seletor_Register_Bank;
     
-    
+    Register_Bank : process (bus_c,write_reg_enable,c_addr) --Banco de registradores
+    begin
+       case  a_addr is  --
+            when "00" => bus_a <= register_0;
+            when "01" => bus_a <= register_1;
+            when "10" => bus_a <= register_2;
+            when "11" => bus_a <= register_3;
+       end case;
+       case  b_addr is  --
+            when "00" => bus_b <= register_0;
+            when "01" => bus_b <= register_1;
+            when "10" => bus_b <= register_2;
+            when "11" => bus_b <= register_3;
+       end case;
+    if (write_reg_enable='1') then
+       case  c_addr is  --
+            when "00" => register_0 <= bus_c;
+            when "01" => register_1 <= bus_c;
+            when "10" => register_2 <= bus_c;
+            when "11" => register_3 <= bus_c;
+       end case;   
+    end if;
+    end process Register_Bank;
     
 
 end rtl;
